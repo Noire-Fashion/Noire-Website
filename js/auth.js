6e5b242b3868be6ed81c7b81348afa111a1335ff
Noire-Fashion <sohamdesigns0407@gmail.com>
2026-02-11 23:12:01 +0530

    Refactor authentication to use loginWithGoogle function

--- js/auth.js
+++ js/auth.js
@@ -1,61 +1,10 @@
-// js/auth.js
+import { supabase } from './supabaseClient.js'
 
-window.auth = {
-  isLoggedIn: false,
-
-  async init() {
-    // IMPORTANT: handle OAuth redirect
-    await window.supabaseClient.auth.getSession()
-
-    const { data: { session } } = await window.supabaseClient.auth.getSession()
-
-    this.isLoggedIn = !!session
-    this.render()
-  },
-
-  render() {
-    const container = document.getElementById("auth-links")
-    if (!container) return
-
-    if (this.isLoggedIn) {
-      container.innerHTML = `
-        <a href="#" id="logout-link">Logout</a>
-      `
-
-      document.getElementById("logout-link").addEventListener("click", async (e) => {
-        e.preventDefault()
-        await window.supabaseClient.auth.signOut()
-        location.reload()
-      })
-
-    } else {
-      container.innerHTML = `
-        <a href="#" id="login-link">Login</a>
-      `
-
-      document.getElementById("login-link").addEventListener("click", async (e) => {
-        e.preventDefault()
-
-        await window.supabaseClient.auth.signInWithOAuth({
-          provider: "google",
-          options: {
-            redirectTo: "https://noire-fashion.github.io/Noire-Website/"
-          }
-        })
-      })
+async function loginWithGoogle() {
+  await supabase.auth.signInWithOAuth({
+    provider: 'google',
+    options: {
+      redirectTo: 'https://noire-fashion.github.io/Noire-Website/'
     }
-  },
-
-  async requireLogin(callback) {
-    if (this.isLoggedIn) {
-      await callback()
-    } else {
-      await window.supabaseClient.auth.signInWithOAuth({
-        provider: "google",
-        options: {
-          redirectTo: "https://noire-fashion.github.io/Noire-Website/"
-        }
-      })
-    }
-  }
+  })
 }
