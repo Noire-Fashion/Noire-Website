bec8ee3c6fa7f1f75d1722a011bbeff23356573d
Noire-Fashion <sohamdesigns0407@gmail.com>
2026-02-11 22:47:01 +0530

    Refactor Google login to use event listener

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,15 @@
+
 import { supabase } from './supabaseClient.js'
 
-async function loginWithGoogle() {
-  await supabase.auth.signInWithOAuth({
-    provider: 'google',
-    options: {
-      redirectTo: 'https://noire-fashion.github.io/Noire-Website/'
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("google-login")
+
+  btn.addEventListener("click", async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: 'https://noire-fashion.github.io/Noire-Website/'
+      }
+    })
   })
-}
+})
